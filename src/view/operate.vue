ec24c7ecebe78dfc8e69a85dec36fbb4c4432171
littleredhat1997 <1656704949@qq.com>
2021-12-24 04:13:34 +0800

    update

--- src/view/operate.vue
+++ src/view/operate.vue
@@ -4,39 +4,42 @@
     <van-loading v-if="loading" />
     <van-tabs animated>
       <van-tab title="生长操作">
-        <van-list finished-text="没有更多了">
-          <van-cell-group v-for="(item, i) in growList" :key="i" :title="'# ' + (i+1) + ': ' + item.createTime">
+        <van-swipe>
+          <van-swipe-item v-for="(item, i) in growList" :key="i">
             <image-preview :image="item.image" />
             <van-cell title="温度" :value="item.temperature" />
             <van-cell title="湿度" :value="item.humidity" />
             <van-cell title="光照" :value="item.light" />
             <van-cell title="备注" :value="item.remark" />
+            <van-cell title="时间" :value="item.createTime" />
             <van-cell title="交易哈希" :value="item.transHash" is-link :to="'transDetail?transHash='+item.transHash" />
             <van-cell title="发送方" :value="item.transFrom" is-link :to="'browser?transFrom='+item.transFrom" />
-          </van-cell-group>
-        </van-list>
+          </van-swipe-item>
+        </van-swipe>
       </van-tab>
       <van-tab title="农事操作">
-        <van-list finished-text="没有更多了">
-          <van-cell-group v-for="(item, i) in farmList" :key="i" :title="'# ' + (i+1) + ': ' + item.createTime">
+        <van-swipe>
+          <van-swipe-item v-for="(item, i) in farmList" :key="i">
             <image-preview :image="item.image" />
             <van-cell title="内容" :value="item.content" />
             <van-cell title="备注" :value="item.remark" />
+            <van-cell title="时间" :value="item.createTime" />
             <van-cell title="交易哈希" :value="item.transHash" is-link :to="'transDetail?transHash='+item.transHash" />
             <van-cell title="发送方" :value="item.transFrom" is-link :to="'browser?transFrom='+item.transFrom" />
-          </van-cell-group>
-        </van-list>
+          </van-swipe-item>
+        </van-swipe>
       </van-tab>
       <van-tab title="加工操作">
-        <van-list finished-text="没有更多了">
-          <van-cell-group v-for="(item, i) in processList" :key="i" :title="'# ' + (i+1) + ': ' + item.createTime">
+        <van-swipe>
+          <van-swipe-item v-for="(item, i) in processList" :key="i">
             <image-preview :image="item.image" />
             <van-cell title="内容" :value="item.content" />
             <van-cell title="备注" :value="item.remark" />
+            <van-cell title="时间" :value="item.createTime" />
             <van-cell title="交易哈希" :value="item.transHash" is-link :to="'transDetail?transHash='+item.transHash" />
             <van-cell title="发送方" :value="item.transFrom" is-link :to="'browser?transFrom='+item.transFrom" />
-          </van-cell-group>
-        </van-list>
+          </van-swipe-item>
+        </van-swipe>
       </van-tab>
     </van-tabs>
   </div>
@@ -45,10 +48,10 @@
 <script>
 import {
   Cell,
-  CellGroup,
-  List,
   Loading,
   NavBar,
+  Swipe,
+  SwipeItem,
   Tab,
   Tabs
 } from 'vant';
@@ -57,10 +60,10 @@ import { listGrowByCode, listFarmByCode, listProcessByCode } from '../api/trace'
 export default {
   components: {
     [Cell.name]: Cell,
-    [CellGroup.name]: CellGroup,
-    [List.name]: List,
     [Loading.name]: Loading,
     [NavBar.name]: NavBar,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
     [Tab.name]: Tab,
     [Tabs.name]: Tabs
   },
@@ -81,7 +84,7 @@ export default {
       Promise.all([
         listGrowByCode(this.code).then(res => { this.growList = res.data }),
         listFarmByCode(this.code).then(res => { this.farmList = res.data }),
-        listProcessByCode(this.code).then(res => { this.processList = res.data }),
+        listProcessByCode(this.code).then(res => { this.processList = res.data })
       ]).then(() => {
         this.loading = false
       })
